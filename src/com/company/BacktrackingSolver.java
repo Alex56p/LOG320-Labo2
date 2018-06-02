5bb0355d60f047813bfefdd80f6870f8c19350a5
Anthony Labelle-Voyez <antho.voyez@hotmail.com>
2018-06-02 03:29:00 -0400

    I'm dead inside

--- src/com/company/BacktrackingSolver.java
+++ src/com/company/BacktrackingSolver.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import javafx.util.Pair;
+import org.omg.CORBA.Current;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,16 +15,38 @@ public class BacktrackingSolver {
     private final int UNASSIGNED_VALUE = 0;
 
     private List<List<Integer>> grid = new ArrayList<>();
+    List<List<List<Integer>>> possibilities = new ArrayList<>();
     private int numberOfCall = 0;
 
     public BacktrackingSolver(String filePath) {
         try {
             for(String line : Files.readAllLines(Paths.get(filePath))) {
-                List<Integer> numbers = new ArrayList<>();
+                List<Integer> numbers = new ArrayList<>(9);
                 for (String number : line.split("(?!^)")) {
                     numbers.add(Integer.parseInt(number));
                 }
                 grid.add(numbers);
+            }
+
+            for (int i = 0; i < 9; ++i)
+            {
+                possibilities.add(new ArrayList<>());
+
+                for (int j = 0; j < 9; ++j)
+                {
+                    possibilities.get(i).add(new ArrayList<>());
+
+                    if (grid.get(i).get(j) == UNASSIGNED_VALUE)
+                    {
+                        for (int k = 1; k <= 9; ++k)
+                        {
+                            if (isValid(i, j, k))
+                            {
+                                possibilities.get(i).get(j).add(k);
+                            }
+                        }
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,28 +55,178 @@ public class BacktrackingSolver {
 
     public boolean Solve() {
         numberOfCall++;
+        CheckWithPossibilities();
         Pair<Integer, Integer> unassignedCase = findNextUnassignedLocation();
-
         if(unassignedCase == null)
             return true;
 
-        for(int i = MIN_VALUE; i <= MAX_VALUE; i++) {
-            if(isValid(unassignedCase.getKey(), unassignedCase.getValue(), i)) {
-                grid.get(unassignedCase.getKey()).set(unassignedCase.getValue(), i);
+        final int x = unassignedCase.getKey();
+        final int y = unassignedCase.getValue();
+
+        for(int i : possibilities.get(x).get(y)) {
+            if(isValid(x, y, i)) {
+                grid.get(x).set(y, i);
 
                 if(Solve()) {
                     return true;
                 }
 
-                grid.get(unassignedCase.getKey()).set(unassignedCase.getValue(), UNASSIGNED_VALUE);
+                grid.get(x).set(y, UNASSIGNED_VALUE);
             }
         }
-
         return false;
     }
 
+    private void CheckWithPossibilities(){
+        for(int i = 0; i < MAX_VALUE; i++) {
+            for (int j = 0; j < MAX_VALUE; j++) {
+                if(possibilities.get(i).get(j).size() == 1){
+                    final int possibility = possibilities.get(i).get(j).get(0);
+                    grid.get(i).set(j, possibility);
+                    RemovePossibility(i,j, possibility);
+                }
+                else {
+                    CheckLinePossibility(i);
+                    CheckColumnPossibility(j);
+                    CheckSectionPossibility(i, j);
+                }
+            }
+        }
+    }
+
+    private void CheckLinePossibility(int x) {
+        final List<List<Integer>> CurrentRow = possibilities.get(x);
+        List<Integer> CheckedNumbers = new ArrayList<>();
+        List<Pair<Pair<Integer, Integer>, Integer>> toRemove = new ArrayList<>();
+        for(int i = 0; i < MAX_VALUE; i++){
+            for (int k : CurrentRow.get(i)) {
+                if (!CheckedNumbers.contains(k) && isOnlyLinePossibility(x, i, k)) {
+                    grid.get(x).set(i, k);
+                    toRemove.add(new Pair<>(new Pair<>(x, i), k));
+                }
+                CheckedNumbers.add(k);
+            }
+            for (Pair<Pair<Integer, Integer>, Integer> aToRemove : toRemove) {
+                RemovePossibility(aToRemove.getKey().getKey(), aToRemove.getKey().getValue(), aToRemove.getValue());
+            }
+        }
+    }
+
+    private void CheckColumnPossibility(int y) {
+        List<Integer> CheckedNumbers = new ArrayList<>();
+        List<Pair<Pair<Integer, Integer>, Integer>> toRemove = new ArrayList<>();
+        for(int i = 0; i < MAX_VALUE; i++){
+            for (int k : possibilities.get(i).get(y)) {
+                if (!CheckedNumbers.contains(k) && isOnlyColumnPossibility(y, i, k)) {
+                    grid.get(i).set(y, k);
+                    toRemove.add(new Pair<>(new Pair<>(i, y), k));
+                }
+                CheckedNumbers.add(k);
+            }
+            for (Pair<Pair<Integer, Integer>, Integer> aToRemove : toRemove) {
+                RemovePossibility(aToRemove.getKey().getKey(), aToRemove.getKey().getValue(), aToRemove.getValue());
+            }
+        }
+    }
+
+    private void CheckSectionPossibility(int x, int y) {
+        List<Integer> CheckedNumbers = new ArrayList<>();
+        List<Pair<Pair<Integer, Integer>, Integer>> toRemove = new ArrayList<>();
+        int boxRow = (x / 3) * 3;
+        int boxCol = (y / 3) * 3;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                for (int k : possibilities.get(i + boxRow).get(j + boxCol)) {
+                    if (!CheckedNumbers.contains(k) && isOnlySectionPossibility(x, y, k)) {
+                        grid.get(i + boxRow).set(j + boxCol, k);
+                        toRemove.add(new Pair<>(new Pair<>(i, y), k));
+                    }
+                    CheckedNumbers.add(k);
+                }
+                for (Pair<Pair<Integer, Integer>, Integer> aToRemove : toRemove) {
+                    RemovePossibility(aToRemove.getKey().getKey(), aToRemove.getKey().getValue(), aToRemove.getValue());
+                }
+            }
+        }
+    }
+
+    private boolean isOnlySectionPossibility(int x, int y, int possibility) {
+        int boxRow = (x / 3) * 3;
+        int boxCol = (y / 3) * 3;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if (i == x && j == y)
+                    continue;
+                for (int k : possibilities.get(i + boxRow).get(j + boxCol)) {
+                    if(k == possibility) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isOnlyLinePossibility(int x, int excludedCase, int possibility){
+        for(int i = 0; i < MAX_VALUE; i++) {
+            if(excludedCase == i)
+                continue;
+            for(int n : possibilities.get(x).get(i)){
+                if(n == possibility)
+                    return false;
+            }
+        }
+        return true;
+    }
+    private boolean isOnlyColumnPossibility(int y, int excludedCase, int possibility){
+        for(int i = 0; i < MAX_VALUE; i++) {
+            if(excludedCase == i)
+                continue;
+            for(int n : possibilities.get(i).get(y)){
+                if(n == possibility)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private void RemovePossibility(int x, int y, int possibility){
+        RemovePossibilityFromCase(x,y);
+        RemovePossibilityFromLine(x, possibility);
+        RemovePossibilityFromColumn(y, possibility);
+        RemovePossibilityFromSection(x, y, possibility);
+    }
+
+    private void RemovePossibilityFromCase(int x, int y){
+        Iterator<Integer> iter = possibilities.get(x).get(y).iterator();
+        while(iter.hasNext()){
+            int possibility = iter.next();
+            iter.remove();
+        }
+    }
+
+    private void RemovePossibilityFromLine(int x, int possibility){
+        for(int i = 0; i < MAX_VALUE; i++){
+            possibilities.get(x).get(i).removeIf(n -> n == possibility);
+        }
+    }
+
+    private void RemovePossibilityFromColumn(int y, int possibility){
+        for(int i = 0; i < MAX_VALUE; i++){
+            possibilities.get(i).get(y).removeIf(n -> n == possibility);
+        }
+    }
+
+    private void RemovePossibilityFromSection(int x, int y, int possibility){
+        int boxRow = (x / 3) * 3;
+        int boxCol = (y / 3) * 3;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                possibilities.get(i+boxRow).get(j+boxCol).removeIf(n -> n == possibility);
+            }
+        }}
     /**
-     * Returns the next unassined case in the grid. Returns null if all the cases are assigned.
+     * Returns the next unassigned case in the grid. Returns null if all the cases are assigned.
      * @return
      */
     public Pair<Integer, Integer> findNextUnassignedLocation() {
@@ -116,6 +289,22 @@ public class BacktrackingSolver {
         }
 
         System.out.println(numberOfCall);
+    }
+
+    public void PrintPossibilities() {
+        for(List<List<Integer>> row : possibilities) {
+            StringBuilder stringRow = new StringBuilder();
+            for(List<Integer> col : row) {
+                stringRow.append(' ');
+                if(col.size() == 0)
+                    stringRow.append('X');
+                for(int number : col) {
+                    stringRow.append(number);
+                }
+            }
+
+            System.out.println(stringRow);
+        }
     }
 
     //Fonctione qui vérifier que la grille complète est valide.
